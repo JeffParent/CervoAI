5b3356a4ef26d38611010fc69382b7933d9fb7a8
Léandre Gagnon-Lewis <leandregagnonlewis@gmail.com>
2019-11-20 17:55:38 -0500

    add extenstion

--- proto.py
+++ proto.py
@@ -4,9 +4,9 @@ import numpy as np
 def load_data():
     X = list()
     for i in range(8):
-        X.append(np.load("data/binary/binary_%s"%i))
+        X.append(np.load("data/binary/binary_%s.npy"%i))
     X = np.array(X)
-    Y = np.load("data/binary/binary_y")
+    Y = np.load("data/binary/binary_y.npy")
     return X,Y
 
 
