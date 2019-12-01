e785b4cef081d2e08950c6026166e36413f81d18
Ratchy <33268194+Ratchy@users.noreply.github.com>
2019-11-30 19:28:49 -0500

    U_net

--- CervoDataset.py
+++ CervoDataset.py
@@ -1,12 +1,13 @@
 import os
-
+import time
 import torch
 from skimage import io
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from torchvision import transforms
+import cv2
 
 
 class CervoDataset(Dataset):
@@ -24,54 +25,110 @@ class CervoDataset(Dataset):
         self.transform = transform
 
     def __len__(self):
-        return len(self.labels)
+        return 5 #len(self.labels)*20
 
+    def extract_image(self, img_folder_path, idx):
+        file = os.listdir(img_folder_path)[idx%20]
+        filename = os.fsdecode(file)
+        if filename[-3:] == "png":
+            img_name = os.path.join(img_folder_path, filename)
+            image = io.imread(img_name)
+            return image
+        
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        img_folder_path = os.path.join(self.root_dir, self.labels.iloc[idx, 0], "Coronal", "merged", "")
-        images = list()
-        for file in os.listdir(img_folder_path):
-            filename = os.fsdecode(file)
-            if filename[-3:] == "png":
-                img_name = os.path.join(self.root_dir, self.labels.iloc[idx, 0], "Coronal", "merged", filename)
-                image = io.imread(img_name)
-                if self.transform is not None:
-                    trans = transforms.Compose([transforms.ToTensor()]+self.transform)
-                    images.append(trans(image))
-                else:
-                    images.append(transforms.ToTensor()(image))
-        X = torch.stack(images)
-        X = X.flatten(end_dim=1)
-        y = self.labels.iloc[idx, 1]
+        X_folder_path = os.path.join(self.root_dir, "12648-10464", "Coronal", "t1", "") #self.labels.iloc[int(idx/20), 0] à la place de "12648-10464"
+        y_folder_path = os.path.join(self.root_dir, "12648-10464", "Coronal", "labels", "")
+        
+        X = self.extract_image(X_folder_path, idx)
+        X = X[:,:,:3]
+        y = self.extract_image(y_folder_path, idx)
+        y = y[:,:,:3]
 
+        if self.transform is not None:
+            trans = transforms.Compose([transforms.ToTensor()]+self.transform)
+            X = (trans(X))
+            y = (trans(y))
+        else:
+            X = transforms.ToTensor()(X)
+            y = transforms.ToTensor()(y)
+
+
+        print(X.shape,y.shape)
         return X, y
 
 
+class u_net():
+    def __init__(self, csv_path, data_path, trained_model = None):
+        self.cervo_dataset = CervoDataset(csv_file=csv_path, root_dir=data_path)
+        if trained_model != None:
+            self.model = trained_model
+
+    def train(self, nb_epoch, learning_rate, momentum, batch_size):
+        self.cervo_loader = DataLoader(self.cervo_dataset, batch_size=batch_size)
+        
+        self.model = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet', in_channels=3, out_channels=3, init_features=32, pretrained=False)
+
+        self.model.to("cpu")
+
+        criterion = torch.nn.MSELoss()
+        optimizer = torch.optim.SGD(self.model.parameters(), lr = learning_rate, momentum=momentum)
+
+        self.model.train()
+        
+        for i_epoch in range(nb_epoch):
+
+            start_time, train_losses = time.time(), []
+            for i_batch, batch in enumerate(self.cervo_loader):
+                images, targets = batch
+                images = images.to("cpu")
+                targets = targets.to("cpu")
+
+                # ===================forward=====================
+                optimizer.zero_grad()
+                predictions = self.model(images)
+                loss = criterion(predictions, targets)
+                
+                # ===================backward=====================
+                loss.backward()
+                optimizer.step()
+                
+
+                train_losses.append(loss.item())
+
+            print(' [-] epoch {:4}/{:}, train loss {:.6f} in {:.2f}s'.format(
+                i_epoch+1, nb_epoch, np.mean(train_losses), time.time()-start_time))
+
+        return self.model
+
+
+    def predict(self, image_index):
+        self.model.eval()
+        image, label = self.cervo_dataset.__getitem__(image_index)
+        image = (image.unsqueeze(0)).to("cpu")
+        label = (label.unsqueeze(0)).to("cpu")
+        with torch.no_grad():
+            prediction = self.model(image)
+        return image[0].permute(1, 2, 0).numpy(), prediction.detach()[0].permute(1, 2, 0).numpy(), label[0].permute(1, 2, 0).numpy()
+        
+
+
 if __name__ == '__main__':
+        unet = u_net(csv_path = 'data/raw/AI_FS_QC_img/data_AI_QC.csv',
+                      data_path = 'data/raw/AI_FS_QC_img/',
+                      trained_model = None)
 
-    cervo_dataset = CervoDataset(csv_file='data/raw/AI_FS_QC_img/data_AI_QC.csv', root_dir='data/raw/AI_FS_QC_img/')
+        trained = unet.train(nb_epoch = 1, learning_rate = 0.01, momentum = 0.99, batch_size = 1)
 
-    fig = plt.figure()
-    c0 = 0
-    c1 = 0
-    for i in range(len(cervo_dataset)):
-        if cervo_dataset[i][1] == 0:
-            c0 += 1
-        else:
-            c1 += 1
-        print("n c0 = {}".format(c0))
-        print("n c1 = {}".format(c1))
+        
+        image, prediction, label = unet.predict(image_index = 2)
+        
+        print(image.shape, prediction.shape, label.shape)
 
-    print("n c0 = {}".format(c0))
-    print("n c1 = {}".format(c1))
-
-
-    for i in range(len(cervo_dataset)):
-        sample = cervo_dataset[i]
-
-        print(i, sample[0].shape, sample[1])
-
-        if i == 3:
-            plt.show()
-            break
+        plt.imshow(image)
+        plt.show()
+        plt.imshow(prediction)
+        plt.show()
+        plt.imshow(label)
+        plt.show()
