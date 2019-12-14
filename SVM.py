f66c87b450c572db6b4fa2825c2c9f49ff0140fd
Ratchy <33268194+Ratchy@users.noreply.github.com>
2019-12-14 16:09:55 -0500

    beautiful code

---
+++ SVM.py
@@ -0,0 +1,125 @@
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as pyplot
+
+class SVM_classifier():
+    def __init__(self):
+        pass
+
+    def optimize_hyperparameters(self, X_train, y_train):
+        '''
+        Optimise les hyperparamètres gamma et C du SVM sur un jeu de validation
+        qui vient du train
+        In
+            X_train
+            y_train
+        Out
+            SVM optimisé avec les bons hyperparamètres
+        '''
+        gamma_list = [0.0001,0.001, 0.005,0.01,0.1,1,10, 100]
+        C_list = [0.01,0.1,1,10,100,1000, 5000, 10000,20000, 100000]
+        maximum_score_SVM = [0,[0,0]]
+        for gamma in gamma_list:
+            for C in C_list:
+                SVM = SVC(gamma = gamma, C = C)
+                SVM.fit(X_train[:500], y_train[:500])
+                score = SVM.score(X_train[500:], y_train[500:])
+                if score > maximum_score_SVM[0]:
+                    maximum_score_SVM[0] = score
+                    maximum_score_SVM[1] = [gamma, C]
+        print("SVM: Meilleurs paramètres: gamma: %s, C: %s" %(maximum_score_SVM[1][0], maximum_score_SVM[1][1]))
+        clf = SVC(gamma=maximum_score_SVM[1][0], C = maximum_score_SVM[1][1], probability=True)
+        self.clf = clf
+        return clf
+
+    def variate_classification_threshold(self, X_test, y_test):
+        '''
+        Calcule le rapel de chaque classe en fonction du seuille de classification
+        In
+            X_test
+            y_test
+        '''
+        zeros_list = []
+        ones_list = []
+        for i in range(0,40):
+            y_pred_proba = self.clf.predict_proba(X_test)
+            def predict(y_pred_proba, seuil = 0.5):
+                y_pred = np.zeros((len(y_pred_proba)))
+                y_pred[np.where(y_pred_proba[:,1] > seuil)] = 1
+                return y_pred
+            y_pred = predict(y_pred_proba, i/40)
+            conf = confusion_matrix(y_test, y_pred)
+            zeros_list.append(conf[0,0]/(conf[0,0]+conf[0,1]))
+            ones_list.append(conf[1,1]/(conf[1,1]+conf[1,0]))
+        zeros_list = np.array(zeros_list)
+        ones_list = np.array(ones_list)
+        a1 = np.reshape(zeros_list, (len(zeros_list),1))
+        a2 = np.reshape(ones_list, (len(ones_list),1))
+        a = np.concatenate((a1,a2),axis = 1)
+        pyplot.plot(np.linspace(0,1,40),a[:,0], label = "Rappel fail")
+        pyplot.plot(np.linspace(0,1,40),a[:,1], label = "Rappel pass")
+        pyplot.plot(np.linspace(0,1,40),np.sum(a,axis = 1)/2, label = "Rappel total")
+        pyplot.ylabel('Rappel')
+        pyplot.xlabel('Threshold de classification')
+        pyplot.title("Rappel en fonction du threshold")
+        pyplot.legend()
+        pyplot.show()
+
+    def fit(self,X_train, y_train):
+        self.clf.fit(X_train, y_train)
+
+    def score(self,X_train,y_train,X_test,y_test):
+        '''
+        Calcule le score en train et test
+        '''
+        print("Train score: ", self.clf.score(X_train,y_train))
+        print("Test score: ", self.clf.score(X_test,y_test))
+
+
+def plot_proportion_distribution(X, y):
+    '''
+    Fait un histogramme de la distribution des proportions pour chaque classe
+    In
+        X et y
+    '''
+    num_bins = 20
+    fig, ax = pyplot.subplots()
+    Pass = X[np.where(y == 0)]
+    Fail = X[np.where(y == 1)]
+
+    n, bins, patches = ax.hist(np.reshape(Pass, (len(Pass)*len(Pass[0]),)), num_bins, density=1, label = "Pass distribution")
+    n, bins, patches = ax.hist(np.reshape(Fail, (len(Fail)*len(Fail[0]),)), num_bins, density=1, label = "Fail distribution")
+    pyplot.legend()
+    pyplot.xlabel("Proportion de l'aire de segmentation sur l'aire totale")
+    pyplot.title("Distribution des proportions")
+    pyplot.show()
+
+if __name__ == '__main__':
+    '''
+    Prends les scores calculés pour chaque méthode de segmentation 
+    et les donne à un SVM pour prédire si un cerveau passe ou fail
+    '''
+    X0 = np.load("saves/Axial_zone_1_scores_X.npy")
+    X1 = np.load("saves/Axial_zone_0_scores_X.npy")
+    X2 = np.load("saves/zone_1_scores_X.npy")
+    X3 = np.load("saves/zone_0_scores_X.npy")
+    X4 = np.load("saves/Axial_simple_scores_X.npy")
+    X5 = np.load("saves/Coranal_simple_scores_X.npy")
+
+
+    X = np.concatenate((X0,X1,X2,X3),axis = 1)
+    y = np.load("saves/zone_0_scores_y.npy")
+
+    plot_proportion_distribution(X, y)
+
+    print(X.shape)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, shuffle = True)
+    SVM = SVM_classifier()
+    SVM.optimize_hyperparameters(X_train, y_train)
+    SVM.fit(X_train, y_train)
+    SVM.variate_classification_threshold(X_test, y_test)
+    SVM.score(X_train,y_train,X_test,y_test)
+
--- UnetSegmentation_One_for_all_Trainer.py
+++ UnetSegmentation_One_for_all_Trainer.py
@@ -14,11 +14,10 @@ class CervoDataset(Dataset):
 
     def __init__(self, csv_file, root_dir, index, transform=None):
         """
-        Args:
-            csv_file (string): Path to the csv file with annotations.
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
+        Dataset pour les données
+        In
+            root_dir: dossier qui contient les données
+            index: Nom des cerveaux
         """
         self.index = index
         self.labels = pd.read_csv(csv_file)
@@ -29,6 +28,14 @@ class CervoDataset(Dataset):
         return len(self.index)*20 #len(self.labels)*20
 
     def extract_image(self, img_folder_path, idx):
+        '''
+        Extrait l'image demandée
+        In
+            img_folder_path: dossier du cerveau
+            idx: indexe de l'image à extraire du cerveau
+        Out
+            Image 256x256x3
+        '''
         file = os.listdir(img_folder_path)[idx]
         filename = os.fsdecode(file)
         if filename[-3:] == "png":
@@ -37,6 +44,14 @@ class CervoDataset(Dataset):
             return image
         
     def __getitem__(self, idx):
+        '''
+        Extrait l'image du cerveau demandé
+        In
+            Idx: indexe entre 0 et __len__
+        Out
+            X: Image grise 265x256x3
+            y: Image du label 265x256x3
+        '''
         rest = idx%20
         idx = self.index[int(idx/20)]
         
@@ -58,14 +73,18 @@ class CervoDataset(Dataset):
             X = transforms.ToTensor()(X)
             y = transforms.ToTensor()(y)
 
-        #print(idx, rest)
-        #print(self.labels.iloc[idx, 0])
-        #print(X.shape,y.shape)
         return X, y
 
 
 class u_net():
     def __init__(self, csv_path, data_path, trained_model = None, device = "cpu"):
+        '''
+        In
+            data_path: path vers les images
+            trained_model: modèle unet qui predit la segmetation
+            device = "cpu" ou "cuda"
+            label_idx: zone qu'on veux segmenter
+        '''
         self.csv_path = csv_path
         self.data_path = data_path
         self.device = device
@@ -73,6 +92,14 @@ class u_net():
             self.model = trained_model
 
     def train(self, nb_epoch, learning_rate, momentum, batch_size, train_index):
+        '''
+        Entraîne le modèle unet avec une entrée de 256x256x3 et une sortie de 256x256x3
+        In
+            nb_epoch, learning_rate, momentum, batch_size: hyperparamètres
+            train_index: noms des cerveaux à entrainer
+        Out
+            model entrainé
+        '''
         self.cervo_dataset = CervoDataset(csv_file=self.csv_path, root_dir=self.data_path, index = train_index)
         self.cervo_loader = DataLoader(self.cervo_dataset, batch_size=batch_size, shuffle = True)
         
@@ -111,21 +138,17 @@ class u_net():
                 i_epoch+1, nb_epoch, np.mean(train_losses), time.time()-start_time))
 
         return self.model
-
-
-    def predict(self, image_index, test_index, device):
-        self.device = device
-        self.cervo_dataset = CervoDataset(csv_file=self.csv_path, root_dir=self.data_path, index = test_index)
-        self.model.eval()
-        image, label = self.cervo_dataset.__getitem__(image_index)
-        image = (image.unsqueeze(0)).to(self.device)
-        label = (label.unsqueeze(0)).to(self.device)
-        with torch.no_grad():
-            prediction = self.model(image)
-        return image[0].permute(1, 2, 0).numpy(), prediction.detach()[0].permute(1, 2, 0).numpy(), label[0].permute(1, 2, 0).numpy()
-
      
 def trainTestSplit(dataLen = 7000, trainTestRatio = 0.8):
+    '''
+    Sépare les données en train et test. 
+    In:
+        datalen: nombre de cerveaux à traîter
+        traintestRatio: ration entre train et test
+        csv_file: path vers le csv
+    Out:
+        Array qui contient les noms des cerveaux pour train et test des Pass ainsi que pour les Fails
+    '''
     linspace = np.arange(dataLen)
     np.random.shuffle(linspace)
     train_index = linspace[:int(dataLen*trainTestRatio)]
@@ -135,18 +158,12 @@ def trainTestSplit(dataLen = 7000, trainTestRatio = 0.8):
     
 
 if __name__ == '__main__':
-        unet = u_net(csv_path = 'data/raw/AI_FS_QC_img/data_AI_QC.csv', data_path = 'data/raw/AI_FS_QC_img/', device = "cuda", trained_model = None)
+    '''
+    Entraîne un modèle unet pour tout le cerveau.
+    '''
+    unet = u_net(csv_path = 'data/raw/AI_FS_QC_img/data_AI_QC.csv', data_path = 'data/raw/AI_FS_QC_img/', device = "cuda", trained_model = None)
 
-        train_index, test_index = trainTestSplit(dataLen = 7180, trainTestRatio = 0.95)
-        
-        trained = unet.train(nb_epoch = 10, learning_rate = 0.01, momentum = 0.99, batch_size = 32, train_index = train_index)
-        torch.save(trained.state_dict(), "models/model0")
-
-        gray, prediction, label = unet.predict(image_index = 2, test_index = test_index, device = "cpu")
-        
-        plt.imshow(gray)
-        plt.show()
-        plt.imshow(prediction)
-        plt.show()
-        plt.imshow(label)
-        plt.show()
+    train_index, test_index = trainTestSplit(dataLen = 7180, trainTestRatio = 0.95)
+    
+    trained = unet.train(nb_epoch = 10, learning_rate = 0.01, momentum = 0.99, batch_size = 32, train_index = train_index)
+    torch.save(trained.state_dict(), "models/model0")
