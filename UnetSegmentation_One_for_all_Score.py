bff3515c738330f460fac8bfd7f6eb4201af4f0d
Ratchy <33268194+Ratchy@users.noreply.github.com>
2019-12-15 14:54:22 -0500

    Update UnetSegmentation_One_for_all_Score.py

--- UnetSegmentation_One_for_all_Score.py
+++ UnetSegmentation_One_for_all_Score.py
@@ -59,8 +59,8 @@ class CervoDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        X_folder_path = os.path.join(self.root_dir, self.index[int(idx/20), 0], "Coronal", "t1", "") #self.index[int(idx/20), 0] à la place de "12648-10464"
-        y_folder_path = os.path.join(self.root_dir, self.index[int(idx/20), 0], "Coronal", "labels", "")
+        X_folder_path = os.path.join(self.root_dir, self.index[int(idx/20), 0], "Axial", "t1", "") #self.index[int(idx/20), 0] à la place de "12648-10464"
+        y_folder_path = os.path.join(self.root_dir, self.index[int(idx/20), 0], "Axial", "labels", "")
         
         X = self.extract_image(X_folder_path, rest)
         X = X[:,:,:3]
